b93df6d195be7201511f54324980b8b64375719f
Ramaswamy Venkatachalam <s7ravenk@uni-bonn.de>
2021-02-24 16:27:18 +0100

    first commit

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(name='sgan',
+      version='1.0',
+      description='Social Gan python package',
+      url='#',
+      author='ramsay',
+      author_email='s7ravenk@uni-bonn.de',
+      license='MIT',
+      install_requires=['numpy', 'torch'],
+      packages=find_packages()
+      )
